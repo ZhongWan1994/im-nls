702805bef780219d7cebead1804e3f0dba969312
wz <360128415@qq.com>
2026-01-17 17:24:11 +0800

    新增查询接口

--- business/src/main/java/com/wz/nls/business/controller/TestController.java
+++ business/src/main/java/com/wz/nls/business/controller/TestController.java
@@ -1,9 +1,12 @@
 package com.wz.nls.business.controller;
 
+import com.wz.nls.business.domain.Demo;
 import com.wz.nls.business.service.DemoService;
 import jakarta.annotation.Resource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class TestController {
@@ -18,5 +21,10 @@ public class TestController {
     @GetMapping("/count")
     public long count() {
         return demoService.count();
+    }
+
+    @GetMapping("/query")
+    public List<Demo> query(String mobile) {
+        return demoService.query(mobile);
     }
 }
--- business/src/main/java/com/wz/nls/business/service/DemoService.java
+++ business/src/main/java/com/wz/nls/business/service/DemoService.java
@@ -1,15 +1,29 @@
 package com.wz.nls.business.service;
 
+import com.wz.nls.business.domain.Demo;
+import com.wz.nls.business.domain.DemoExample;
 import com.wz.nls.business.mapper.DemoMapper;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DemoService {
 
     @Resource
-      private DemoMapper demoMapper;
+    private DemoMapper demoMapper;
     public long count() {
         return demoMapper.countByExample(null);
+    }
+
+    public List<Demo> query(String mobile) {
+        DemoExample example = new DemoExample();
+        example.setOrderByClause("id desc");
+        DemoExample.Criteria criteria = example.createCriteria();
+        if (mobile != null) {
+            criteria.andMobileNameEqualTo(mobile);
+        }
+        return demoMapper.selectByExample(example);
     }
 }
