68a475a875f5d20f0aa4f44672e35c43cefd1ca6
wz <360128415@qq.com>
2026-01-18 11:46:22 +0800

    集成ant-design-Vue

--- web/src/main.js
+++ web/src/main.js
@@ -1,5 +1,8 @@
-import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import 'ant-design-vue/dist/reset.css'
+import Antd from 'ant-design-vue'
+import { createApp } from 'vue'
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(Antd).mount('#app');
