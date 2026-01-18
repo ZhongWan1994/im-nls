6b804b7bd39327e1f8c9b0b09fc8f0f5f962e2d9
wz <360128415@qq.com>
2026-01-18 15:07:43 +0800

    引入并使用路由；新增icon库

--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,15 @@
-import './style.css'
-import App from './App.vue'
-import 'ant-design-vue/dist/reset.css'
-import Antd from 'ant-design-vue'
 import { createApp } from 'vue'
+import App from './App.vue'
+import * as Icons from '@ant-design/icons-vue'
+import Antd from 'ant-design-vue'
+import router from './router'
+import './style.css'
+import 'ant-design-vue/dist/reset.css'
 
 const app = createApp(App);
-app.use(Antd).mount('#app');
+app.use(Antd).use(router).mount('#app');
+
+const icons = Icons
+for (const i in icons) {
+    app.component(i, icons[i])
+}
