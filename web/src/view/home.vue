c63d49f3c2143d2d71a06e99d03ede6f84a1ef33
wz <360128415@qq.com>
2026-01-18 15:26:09 +0800

    抽取header组件，公共化

--- web/src/view/home.vue
+++ web/src/view/home.vue
@@ -1,18 +1,6 @@
 <template>
   <a-layout>
-    <a-layout-header class="header">
-      <div class="logo"/>
-      <a-menu
-          v-model:selectedKeys="selectedKeys1"
-          theme="dark"
-          mode="horizontal"
-          :style="{ lineHeight: '64px' }"
-      >
-        <a-menu-item key="1">nav 1</a-menu-item>
-        <a-menu-item key="2">nav 2</a-menu-item>
-        <a-menu-item key="3">nav 3</a-menu-item>
-      </a-menu>
-    </a-layout-header>
+    <the-header></the-header>
     <a-layout-content style="padding: 0 50px">
       <a-breadcrumb style="margin: 16px 0">
         <a-breadcrumb-item>Home</a-breadcrumb-item>
@@ -78,26 +66,14 @@
 
 <script setup>
 import { ref } from 'vue';
-const selectedKeys1 = ref(['2']);
+import TheHeader from "../components/the-header.vue";
 const selectedKeys2 = ref(['1']);
 const openKeys = ref(['sub1']);
 </script>
 
 <style scoped>
-.logo {
-  float: left;
-  width: 120px;
-  height: 31px;
-  margin: 16px 24px 16px 0;
-  background: rgba(255, 255, 255, 0.3);
-}
-
 .ant-row-rtl .logo {
   float: right;
   margin: 16px 0 16px 24px;
-}
-
-.site-layout-background {
-  background: #fff;
 }
 </style>
