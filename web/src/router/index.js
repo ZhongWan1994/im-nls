9589e2c9a840b701f39d38a6c174ab3c7c83261b
wz <360128415@qq.com>
2026-01-18 15:12:46 +0800

    使用路由 新增登录页

--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,10 +1,17 @@
 import {createRouter, createWebHistory} from "vue-router"
-import Home from "../view/Home.vue"
+import Home from "../view/home.vue"
+import Login from "../view/login.vue"
 
-const routes = [{
-    path: "/home",
-    component: Home
-}]
+const routes = [
+    {
+        path: "/home",
+        component: Home
+    },
+    {
+        path: "/login",
+        component: Login
+    }
+]
 
 const router = createRouter({
     history: createWebHistory(),
---
+++ web/src/view/login.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+ 登录页
+</template>
+
+<style scoped>
+
+</style>
