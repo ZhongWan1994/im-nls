d54fc35d5af056f70183d701af3747323365f041
wz <360128415@qq.com>
2026-01-20 19:35:17 +0800

    新增拦截器

--- web/src/main.js
+++ web/src/main.js
@@ -5,6 +5,7 @@ import Antd from 'ant-design-vue'
 import router from './router'
 import './style.css'
 import 'ant-design-vue/dist/reset.css'
+import axios from "axios";
 
 const app = createApp(App);
 app.use(Antd).use(router).mount('#app');
@@ -13,3 +14,18 @@ const icons = Icons
 for (const i in icons) {
     app.component(i, icons[i])
 }
+
+axios.interceptors.request.use(function (config) {
+    console.log("请求参数", config)
+    return config
+}, error=>{
+    return Promise.reject(error)
+})
+
+axios.interceptors.response.use(function (response) {
+    console.log("返回结果", response)
+    return response
+}, error=>{
+    console.log("返回错误", error)
+    return Promise.reject(error)
+})
